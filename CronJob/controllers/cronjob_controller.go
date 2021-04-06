23f91533ae28360b529700033b2cf6029eeda013
zhuang <chicheng.zheng@mail.mcgill.ca>
2021-04-06 13:46:36 +0800

    done

--- CronJob/controllers/cronjob_controller.go
+++ CronJob/controllers/cronjob_controller.go
@@ -18,12 +18,12 @@ package controllers
 
 import (
 	"context"
-	"fmt"
+	//"fmt"
 	"sort"
 	"time"
 
 	"github.com/go-logr/logr"
-	"github.com/robfig/cron"
+	//"github.com/robfig/cron"
 	kbatch "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
